a38cad1a93a118bdbd00ece81d89c38b0e8d087c
Nysed17 <nysedilsasso@gmail.com>
2022-10-20 15:05:40 +0200

    llll

---
+++ testfiles/test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class MyClass {
+    int arg0;
+    float* arg1;
+    MyClass(int arg0, float* arg1){
+        this->arg0 = arg0;
+        this->arg1 = arg1;
+    }
+
+    int fn(int arg2, char* arg3){
+        return arg2 + arg3[0];
+    }
+};
+
+struct mystr {
+    int argc;
+};
+
+template<typename T>
+void fn(T arg) {
+    return std::vector<T>({arg});
+}
+
+int main(int argc, char* argv[]){
+
+    std::cout<<argv[argc-1]<<std::endl;
+
+    std::vector<int> vec;
+    struct mystr a;
+    a.argc = 0;
+
+    sort(vec.begin(), vec.end(), [argc](const int & a, const int & b) -> bool { 
+        int c = b - argc;
+        return a > c;
+    });
+
+    return 0;
+}
